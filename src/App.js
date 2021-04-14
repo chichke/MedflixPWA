c65d033b712883b87d2a395db91327161eec7522
Bastien Silhol <bastien.silhol@epitech.eu>
2021-04-15 00:26:51 +0200

    CI CD NOW WORKING

--- src/App.js
+++ src/App.js
@@ -8,57 +8,55 @@ import fetchTodos from "./service/api";
 const LOCAL_STORAGE_KEY = "todo-list";
 
 function App() {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    useEffect(() => {
-        fetchTodos()
-            .then(result => {
-                setTodos(result);
-            });
+  useEffect(() => {
+    fetchTodos().then((result) => {
+      setTodos(result);
+    });
+  }, []);
 
-    }, []);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-    }, [todos]);
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
 
-    function addTodo(todo) {
-        setTodos([todo, ...todos]);
+  function toggleComplete(id) {
+    if (todos) {
+      setTodos(
+        todos.map((todo) => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            };
+          }
+          return todo;
+        })
+      );
     }
+  }
 
-    function toggleComplete(id) {
-        if (todos) {
-            setTodos(
-                todos.map(todo => {
-                    if (todo.id === id) {
-                        return {
-                            ...todo,
-                            completed: !todo.completed
-                        };
-                    }
-                    return todo;
-                })
-            );
-        }
-    }
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
 
-    function removeTodo(id) {
-        setTodos(todos.filter(todo => todo.id !== id));
-    }
-
-    return (
-        <div className="App">
-            <Typography style={{ padding: 16 }} variant="h1">
-                React Todo
-            </Typography>
-            <TodoForm addTodo={addTodo} />
-            <TodoList
-                todos={todos}
-                removeTodo={removeTodo}
-                toggleComplete={toggleComplete}
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Typography style={{ padding: 16 }} variant="h1">
+        React Medflix
+      </Typography>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        removeTodo={removeTodo}
+        toggleComplete={toggleComplete}
+      />
+    </div>
+  );
 }
 
 export default App;
